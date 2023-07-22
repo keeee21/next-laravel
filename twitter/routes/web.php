d8cf6eda58a4a2b9680eb39fd54c308e6aed9d98
Keisuke Uemoto <keisuke.u211016@gmail.com>
2023-07-23 00:11:55 +0900

    breezeapi

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
