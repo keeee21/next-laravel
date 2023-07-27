21d1affd7c4e82fae4988de1cac252885a62d9cb
Keisuke Uemoto <keisuke.u211016@gmail.com>
2023-07-27 23:47:25 +0900

    chakra ui

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import { Providers } from "./providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -11,8 +11,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
