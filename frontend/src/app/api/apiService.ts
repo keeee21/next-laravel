852344467835f4ad53fdaef60e48c8ad213d1681
Keisuke Uemoto <keisuke.u211016@gmail.com>
2023-07-30 23:37:15 +0900

    Topページとツイート機能

--- frontend/src/app/api/apiService.ts
+++ frontend/src/app/api/apiService.ts
@@ -2,20 +2,32 @@ import axios from "axios";
 
 import { LoginParams, RegisterParams } from "../types/auth";
 import { Tweet } from "../types/tweet";
+import { FailedPost, SuccessPost } from "../types/api";
+
+// create an axios instance
+const apiClient = axios.create({
+  baseURL: "http://localhost:8083",
+  withCredentials: true,
+});
 
 export const getCsrfCookie = async () => {
-  return await axios.get("http://localhost:8083/sanctum/csrf-cookie", { withCredentials: true });
+  return await apiClient.get("/sanctum/csrf-cookie");
 };
 
 export const login = async (loginParams: LoginParams) => {
-  return await axios.post("http://localhost:8083/login", loginParams, { withCredentials: true });
+  return await apiClient.post("/login", loginParams);
 };
 
 export const register = async (registerParams: RegisterParams) => {
-  return await axios.post("http://localhost:8083/register", registerParams, { withCredentials: true });
+  return await apiClient.post("/register", registerParams);
 };
 
 export const getAllTweets = async (): Promise<Tweet[]> => {
-  const response = await axios.get("http://localhost:8083/tweets", { withCredentials: true });
+  const response = await apiClient.get("/tweets");
+  return response.data;
+};
+
+export const postTweet = async (tweet: string): Promise<SuccessPost | FailedPost> => {
+  const response = await apiClient.post("/postTweet", { tweet: tweet });
   return response.data;
 };
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,34 +1,54 @@
 "use client";
 
-import { Box, Heading, Input, VStack, Button, Flex, Card } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Box, Heading, Input, VStack, Button, Flex, Card, Alert, AlertIcon } from "@chakra-ui/react";
+import { useCallback, useEffect, useState } from "react";
 
 import { useInput } from "../hooks/useInput";
-import { getAllTweets } from "../api/apiService";
+import { getAllTweets, postTweet } from "../api/apiService";
 import { Tweet } from "../types/tweet";
 
 export default function Dashboard() {
+  const [reloadTweets, setReloadTweets] = useState(false);
+  const [error, setError] = useState("");
   const [tweets, setTweets] = useState<Tweet[]>([]);
   const { value: inputValue, onChange: changeInputValue } = useInput("");
 
-  const handleTweet = () => {
-    // setTweets([inputValue, ...tweets]);
+  // ツイートする
+  const handleTweet = async () => {
+    try {
+      const response = await postTweet(inputValue);
+      if (response.status == "200") {
+        setReloadTweets((prev) => !prev);
+      }
+    } catch (err) {
+      setError("ツイートに失敗しました。再度試してください");
+    }
   };
 
-  useEffect(() => {
-    const fetchTweets = async () => {
+  // ツイートを全件取得する
+  const fetchTweets = useCallback(async () => {
+    try {
       const tweetsFromApi: Tweet[] = await getAllTweets();
-      console.log(tweetsFromApi);
       setTweets(tweetsFromApi);
-    };
-
-    fetchTweets();
+    } catch (err) {
+      setError("ツイートの取得に失敗しました。画面を再読み込みしてください");
+    }
   }, []);
+
+  useEffect(() => {
+    fetchTweets();
+  }, [fetchTweets, reloadTweets]);
 
   return (
     <>
-      <Box maxW="xl" mx="auto" mt="10" bgColor="red.100">
+      <Box maxW="xl" mx="auto" mt="10" bgColor="teal.100">
         <Heading mb="6">X Clone</Heading>
+        {error && (
+          <Alert status="error" borderRadius="md" my="4">
+            <AlertIcon />
+            {error}
+          </Alert>
+        )}
         <VStack spacing="6">
           <Input
             placeholder="What's happening?"
@@ -55,7 +75,9 @@ export default function Dashboard() {
           <Box minW={{ base: "90%", md: "468px" }} textAlign="center">
             {tweets.map((tweet, index) => (
               <Card key={index} mt="3">
-                {tweet.tweet}
+                <Box p="3" textAlign="left">
+                  {tweet.tweet}
+                </Box>
               </Card>
             ))}
           </Box>
